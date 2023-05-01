db7ebe92af6cd890bb5ff3897e1f4bd20145bb74
mat19211 <132259021+mat19211@users.noreply.github.com>
2023-05-01 09:29:41 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Import stylesheets
 import './style.css';
-// test
-// test 2
-// test 4
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
