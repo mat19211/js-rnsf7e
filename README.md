b59777cdd6828bfb35dcc7db39c5f79e16e1a797
mat19211 <132259021+mat19211@users.noreply.github.com>
2023-05-01 11:57:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-rnsf7e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-sp9yxg)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+// test
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
